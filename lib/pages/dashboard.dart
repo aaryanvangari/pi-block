1205e63c3f73889720c5ac87c918b31cdc228968
Aaryan Vangari <aaryanvangari@gmail.com>
2025-12-31 06:34:06 +0530

    feat: responsive dashboard

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:pi_block/widgets/blocking_info_stats.dart';
-
 import 'package:pi_block/widgets/host_info_stats.dart';
 import 'package:pi_block/widgets/session_info_stats.dart';
 import 'package:pi_block/widgets/summary_info_stats.dart';
@@ -12,30 +11,63 @@ class DashboardPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final widgets = const [
+      // DNS Blocking
+      BlockingInfoStats(),
+
+      // Pi-Hole Stats Overview
+      SummaryInfoStats(),
+
+      // Pi-Hole Session Info
+      SessionInfoStats(),
+
+      // Host Information
+      HostInfoStats(),
+
+      // System information
+      SystemInfoStats(),
+
+      // Versions
+      VersionInfoStats(),
+    ];
+
+    int crossAxisCount(double width) {
+      int minimumWidthOfWidget = 350;
+      return (width / minimumWidthOfWidget).toInt();
+    }
+
     return Scaffold(
-      // appBar: AppBar(),
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          children: [
-            // DNS Blocking
-            BlockingInfoStats(),
-
-            // Pi-Hole Stats Overview
-            SummaryInfoStats(),
-
-            // Pi-Hole Session Info
-            SessionInfoStats(),
-
-            // Host Information
-            HostInfoStats(),
-
-            // System information
-            SystemInfoStats(),
-
-            // Versions
-            VersionInfoStats(),
-          ],
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            final width = constraints.maxWidth;
+            if (width < 500) {
+              return Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: widgets,
+                ),
+              );
+            } else {
+              return Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: crossAxisCount(width),
+                    crossAxisSpacing: 8.0, // Space between columns
+                    mainAxisSpacing: 8.0, // Space between rows
+                    childAspectRatio: 1.0,
+                  ),
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: widgets.length,
+                  itemBuilder: (context, index) {
+                    return widgets[index];
+                  },
+                ),
+              );
+            }
+          },
         ),
       ),
     );
